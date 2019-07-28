b7d1d96301466ab2755115c28c16a17dadff7d8c
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-28 20:53:43 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,8 +6,11 @@ def meal_choice(veg1, veg2, protein)
   if protein = nil
     puts "meat"
 
+  #Output two strings
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  
+  #Since "puts" has a return value of nil, need to return a string that includes the guest's options
   
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 end  
